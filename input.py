ff4085f4f722757be68c1017c32b6b02a8df968a
Justin Swistak <jts7382@rit.edu>
2022-03-16 17:59:45 -0400

    added artist insertion sql but don't have access to insert statements.

--- input.py
+++ input.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # music.csv format
 # "artist.familiarity","artist.hotttnesss","artist.id","artist.latitude","artist.location","artist.longitude","artist.name","artist.similar","artist.terms","artist.terms_freq","release.id","release.name","song.artist_mbtags","song.artist_mbtags_count","song.bars_confidence","song.bars_start","song.beats_confidence","song.beats_start","song.duration","song.end_of_fade_in","song.hotttnesss","song.id","song.key","song.key_confidence","song.loudness","song.mode","song.mode_confidence","song.start_of_fade_out","song.tatums_confidence","song.tatums_start","song.tempo","song.time_signature","song.time_signature_confidence","song.title","song.year"
+import sqlconnect
+
 artist_name_idx = 6
 song_duration_idx = 18
 song_title_idx = 33
@@ -20,11 +22,13 @@ def get_indexes():
 if __name__ == "__main__":
     file = open("music.csv")
     file.readline()
+    conn = sqlconnect.connect()
+    cur = conn.cursor()
 
     for line in file:
         line = line.split(",")
-        if line[song_title_idx] != "\"0\"":
-            print(line[artist_name_idx], line[song_duration_idx], line[song_title_idx], line[song_year])
+        print(line[artist_name_idx], line[song_duration_idx])
+        # cur.execute(f"INSERT INTO p320_19.artists(artistid, artist_name) Values (1,'{line[artist_name_idx]}');")
+    cur.execute(f"INSERT INTO p320_19.artists(artistid, artist_name) Values (1,'testing');")
 
     file.close()
-    get_indexes()
