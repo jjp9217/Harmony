febe237f38c3c21aeb39bc17b82427c0d7618617
Justin Swistak <jts7382@rit.edu>
2022-03-16 00:20:27 -0400

    starting working on parsing music.csv but realized it doesn't actually have song names.

--- input.py
+++ input.py
@@ -1,2 +1,30 @@
+#!/usr/bin/env python3
 # music.csv format
 # "artist.familiarity","artist.hotttnesss","artist.id","artist.latitude","artist.location","artist.longitude","artist.name","artist.similar","artist.terms","artist.terms_freq","release.id","release.name","song.artist_mbtags","song.artist_mbtags_count","song.bars_confidence","song.bars_start","song.beats_confidence","song.beats_start","song.duration","song.end_of_fade_in","song.hotttnesss","song.id","song.key","song.key_confidence","song.loudness","song.mode","song.mode_confidence","song.start_of_fade_out","song.tatums_confidence","song.tatums_start","song.tempo","song.time_signature","song.time_signature_confidence","song.title","song.year"
+artist_name_idx = 6
+song_duration_idx = 18
+song_title_idx = 33
+song_year = 34
+
+
+def get_indexes():
+    file = open("music.csv")
+    indexes = file.readline().split(",")
+    i = 0
+    for idx in indexes:
+        print(i, idx)
+        i += 1
+    file.close()
+
+
+if __name__ == "__main__":
+    file = open("music.csv")
+    file.readline()
+
+    for line in file:
+        line = line.split(",")
+        if line[song_title_idx] != "\"0\"":
+            print(line[artist_name_idx], line[song_duration_idx], line[song_title_idx], line[song_year])
+
+    file.close()
+    get_indexes()
