68ce48c48ea581bf5111f638d4fc77a4a5984d48
Justin Swistak <jts7382@rit.edu>
2022-03-17 00:10:19 -0400

    removed with in sqlconnect becuase it was ending the resource after returning the connection

--- input.py
+++ input.py
@@ -2,6 +2,7 @@
 # music.csv format
 # "artist.familiarity","artist.hotttnesss","artist.id","artist.latitude","artist.location","artist.longitude","artist.name","artist.similar","artist.terms","artist.terms_freq","release.id","release.name","song.artist_mbtags","song.artist_mbtags_count","song.bars_confidence","song.bars_start","song.beats_confidence","song.beats_start","song.duration","song.end_of_fade_in","song.hotttnesss","song.id","song.key","song.key_confidence","song.loudness","song.mode","song.mode_confidence","song.start_of_fade_out","song.tatums_confidence","song.tatums_start","song.tempo","song.time_signature","song.time_signature_confidence","song.title","song.year"
 import sqlconnect
+import psycopg2
 
 artist_name_idx = 6
 song_duration_idx = 18
@@ -29,6 +30,11 @@ if __name__ == "__main__":
     #     line = line.split(",")
     #     print(line[artist_name_idx], line[song_duration_idx])
         # cur.execute(f"INSERT INTO p320_19.artists(artistid, artist_name) Values (1,'{line[artist_name_idx]}');")
-    cur.execute("INSERT INTO p320_19.artists(artistid, artist_name) Values (3,'The Look of Love');")
+
+    # cur.execute("INSERT INTO p320_19.artists(artistid, artist_name) Values (3,'testing');")
+
+    cur.execute("SELECT * FROM p320_19.artists")
+    print(cur.)
+
 
     file.close()
