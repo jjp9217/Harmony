c9472c9178003aa3c48fa7569019b895b644aa90
Justin Swistak <jts7382@rit.edu>
2022-03-18 15:22:01 -0400

    deleted excess files

--- harmonySQL.py
+++ harmonySQL.py
@@ -16,6 +16,20 @@ register_sql = "insert into p320_19.\"user\" (username, acc_creation_date, passw
 
 user_exists_sql = "select * from p320_19.\"user\" where username = '%s'"
 
+login_SQL = "select * from p320_19.\"user\" where username = '%s' and password = '%s';"
+
+
+
+# insert into p320_19."user" (username, acc_creation_date, password, first_name, last_name, email)
+# values ('test', '2022-03-17', 'pass', 'catherine', 'katherine', 'null@gmail.com');
+import sqlconnect
+
+register_SQL = "insert into p320_19.\"user\" (username, acc_creation_date, password, first_name, last_name, email)" \
+      "values (%s, %s, %s, %s, %s, %s);"
+
+# TODO
+search_user_SQL = "SELECT * FROM p320_19.dummy;"
+
 
 
 def register():
