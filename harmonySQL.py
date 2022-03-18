7c5de2fd8aee298bf3e1e0beb6cd1beee4f0bc65
psbyd <null>
2022-03-18 15:21:50 -0400

    made initial unique check for registration

--- harmonySQL.py
+++ harmonySQL.py
@@ -1,8 +1,10 @@
 """
 @author Jesse Pingitore
 file: harmonySQL.py
-This file acts as a utility dump for the SQL calls.
+This file acts as a utility dump for the SQL call.
 It holds both the string templates for the calls, and functions for different user actions.
+Note that the actions here are ONLY for user interactions.
+Any developer tools should use a different file.
 """
 import datetime
 
@@ -32,22 +34,26 @@ def register():
 
         ### Build the user input
 
-        username = input("provide a new username: >")
+        username = input("Provide a new username: >")
 
         #Make sure the username is unique -- it is the key for the "users" table
         unique = False
         while not unique:
 
                 cursor.execute(user_exists_sql, username)
-                cursor.commit()
-                flag = True
+                entries = cursor.fetchall()
+                if len(entries) > 0:
+                        print("The username '" + username +"' is taken")
+                        username = input("provide a new username: >")
+                        continue
+                else:
+                        unique = True
 
 
-
-        password = input("provide a new password: >")
-        email = input("provide an email address: >")
-        f_name = input("first name: >")
-        l_name = input("last name: >")
+        password = input("Provide a new password: >")
+        email = input("Provide an email address: >")
+        f_name = input("First name: >")
+        l_name = input("Last name: >")
         current_date = str(datetime.date.today()) #cast date to yyyy-mm-dd string
 
 
