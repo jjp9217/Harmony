e750d4c1a6ad551c344ace9b175ba8bc5779beb0
Justin Swistak <jts7382@rit.edu>
2022-03-31 12:13:43 -0400

    finished adding more users

--- generate.py
+++ generate.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import csv
+
 import sqlconnect
 
 play_song_SQL = "INSERT INTO p320_19.listens(songid, username, datetime) VALUES (%s, %s, %s)"
@@ -13,7 +15,19 @@ cur = conn.cursor()
 def generate_listens():
     return
 
+
 def generate_users():
+    file = open("Data/MOCK_DATA_users.csv")
+    file.readline()
+    parser = csv.reader(file)
+    for user in parser:
+        cur.execute(register_SQL, (user[4], user[5], user[3], user[0], user[1], user[2]))
+        conn.commit()
+
+
 
 
 if __name__ == "__main__":
+    # generate_users()
+
+
